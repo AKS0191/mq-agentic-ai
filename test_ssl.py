65e5209d8e32ea9690b68dc1c926f5ac5b5e28b6
“Atul <atul.kumar.sharma@dnb.no>
2025-09-23 12:09:04 +0200

    test

--- test_ssl.py
+++ test_ssl.py
@@ -1,9 +1,3 @@
-import os
-import certifi
 import httpx
-
-os.environ["SSL_CERT_FILE"] = certifi.where()
-os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
-
 r = httpx.get("https://api.openai.com/v1")
-print(r.status_code)  # Should return 401 if you haven’t set an API key
+print(r.status_code)
